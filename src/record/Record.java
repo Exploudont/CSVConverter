5ba2caee2f1dff0615a530f8cb0b0881670bbfd7
Exploudont <daniele.longobardi@libero.it>
2022-05-06 14:55:56 +0200

    Added CSVReader package

---
+++ src/record/Record.java
@@ -0,0 +1,14 @@
+package record;
+
+/**
+Record interface.
+Rappresent a single row.
+*/
+public interface Record {
+	
+	/**
+	Return the values of camps.
+	@return return the values of camps
+	*/
+	public String[] getStringArray();
+}
